865011e71334ae56c8a8048b84f3380f6b542123
khansuhad <suhadahmodkhan@gmail.com>
2024-03-12 16:21:26 +0600

    student addmission api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT  ;
 
 app.use(cors())
 app.use(express.json())
@@ -23,12 +23,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const admissionCollection = client.db("imcDB").collection("admissions");
+    app.post("/admissionStudents", async (req, res) => {
+        const formInfo = req.body;
+        const result = await admissionCollection.insertOne(formInfo);
+        res.send(result);
+      });
+      app.get("/admissionStudents", async (req, res) => {
+        const result = await admissionCollection.find().toArray();
+        res.send(result)
+      })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
