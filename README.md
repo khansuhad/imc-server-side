5f82b6f47719712fbd92a1d7f7f922c380925a2d
khansuhad <suhadahmodkhan@gmail.com>
2024-03-14 21:46:41 +0600

    readme file added

---
+++ README.md
@@ -0,0 +1,5 @@
+# Infinity Math Center
+
+- [Live link](https://infinity-math-center.web.app/)
+- [Server link link](https://imc-server-side.vercel.app)
+- [Client site link](https://github.com/khansuhad/imc-client-side)
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // addmission student api start
     const admissionCollection = client.db("imcDB").collection("admissions");
     app.post("/admissionStudents", async (req, res) => {
@@ -80,7 +80,7 @@ async function run() {
     })
     // addpayment api end
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
